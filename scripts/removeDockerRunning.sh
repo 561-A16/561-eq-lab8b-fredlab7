31e293226c23f4034ccad30639764d8c100b8f8f
Frederick Laberge <fred.laberge7@gmail.com>
2016-11-01 18:21:36 -0400

    Change docker run command

--- scripts/removeDockerRunning.sh
+++ scripts/removeDockerRunning.sh
@@ -3,4 +3,4 @@ set -ev
 #docker rm $(docker ps -a -q)
 #docker rmi fredlab7/webapp:latest
 docker pull fredlab7/webapp:latest
-docker run -itd -p 6530:5000 fredlab7/webapp:latest
+docker run -it -p 6530:5000 fredlab7/webapp:latest
